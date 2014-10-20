572444bc07a7a12573e03165bca5275c42560609
Cem Ozen <cemalper@gmail.com>
2014-10-19 21:26:54 -0400

    GetData Week 1 Updates

--- GettingCleaningData/Course_Notes.R
+++ GettingCleaningData/Course_Notes.R
@@ -2,7 +2,7 @@
 ############################ GETTING AND CLEANING DATA #########################
 ###############################################################################
 
-### GET/SET Working Directory
+##### GET/SET Working Directory #####
 getwd()
 setwd()
 getwd()
@@ -12,198 +12,300 @@ getwd()
 setwd("./Documents") ##goes to level that you indicate
 getwd()
 ###Absolute
-setwd("C:/Users/ozenca/Documents/Courses")
+setwd("C:/Users/cemalper/Documents/Coursera/GetData/")
 getwd()
 ###Windows machines
-setwd("C:\\Users\\ozenca\\Documents\\Courses")
+setwd("C:\\Users\\cemalper\\Documents\\Coursera\\GetData")
 getwd()
 ###Check if a directory exits
 file.exists("Coursera-Getting and Cleaning Data")
 file.exists("Some Folder")
 ###Create a directory
-dir.create("Sole Folder") ## will create a folder if it doesn't exists
+dir.create("Some Folder") ## will create a folder if it doesn't exists
 ###Example
-setwd("C:\\Users\\ozenca\\Documents\\Courses\\Coursera-Getting and Cleaning Data\\Notes")
+setwd("C:\\Users\\cemalper\\Documents\\Coursera\\GetData")
 getwd()
-if(!file.exists("data")) {
-    dir.create("data")
+if(!file.exists("Data")) {
+    dir.create("Data")
 }
 
-###Download a file from internet
+##### Download a file from internet #####
 download.file()
 ?download.file
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-download.file(fileUrl, destfile="./data/cameras.csv")
-download.file(fileUrl, destfile="./data/cameras.csv", method = "curl") ##for linux because the url is https
-list.files("./data")
+download.file(fileUrl, destfile="./Data/cameras.csv")
+download.file(fileUrl, destfile="./Data/cameras.csv", method = "curl") # For MAC OSX
+download.file(fileUrl, destfile="./Data/cameras.csv", method = "curl") ## for linux because the url is https
+list.files("./Data")
 dateDownloaded <- date()
 dateDownloaded
 
 
-
-
-
-setwd("C:\\Users\\ozenca\\Documents\\Courses\\Coursera-Getting and Cleaning Data\\Notes")
-getwd()
-if(!file.exists("data")) {
-    dir.create("data")
-}
-fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-download.file(fileUrl, destfile="./data/cameras.csv")
-list.files("./data")
-dateDownloaded <- date()
-dateDownloaded
-
-###Loading Flat Files
+##### Loading Flat Files #####
 read.table() ## this is the main function for reading data into R
 # Flexible and robust but requires more parameters
 # Reads data into RAM - big data creates problems
-read.table() - file, header, sep, row.names, nrows
+read.table() # important parameters file, header, sep, row.names, nrows
 # read.csv(), read.csv2()
-cameraData <- read.table("./data/cameras.csv", sep = ",", header = TRUE)
+cameraData <- read.table("./Data/cameras.csv", sep = ",", header = TRUE)
+head(cameraData)
+str(cameraData)
+summary(cameraData)
+
+# read.csv has some predefined parameters
+# such as: automaticly assigns sep = "," and header = TRUE
+cameraData <- read.csv("./data/cameras.csv")
 head(cameraData)
 
-cameraData <- read.csv("./data/cameras.csv") #automaticly assigns sep = "," and header = TRUE
-head(cameraData)
+# Quotes
+# Most of the time if data has ' or ", read.table may not be able to read correctly
+# paramater quote = "" will solve the problem. This means no quotes
 
+# na.strings paramater will set the character used for missing data
+
+# nrows paramater will determine the number of rows to be read by read.table
 cameraData <- read.csv("./data/cameras.csv", nrows = 4)
 head(cameraData)
 
+# skip parameter will determine the number of lines to skip
 cameraData <- read.csv("./data/cameras.csv", skip = 2, nrows = 4)
 head(cameraData)
 
+
+##### Reading Excel Files #####
+if(!file.exists("Data")) {
+  dir.create("Data")
+}
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
-download.file(fileUrl, destfile="./data/cameras.xlsx", method = "internal")
-list.files("./data")
+download.file(fileUrl, destfile="./Data/cameras.xlsx")
+list.files("./Data")
 dateDownloaded <- date()
 dateDownloaded
 
+# The package we will use is xlsx package
+install.packages("xlsx")
 library(xlsx)
-cameraData <- read.xlsx("./data/Baltimore_Fixed_Speed_Cameras.xlsx", sheetIndex = 1, header = TRUE)
+list.files("./Data")
+# We would like to get the first sheet and we should consider that the first row is the header row
+cameraData <- read.xlsx("./Data/Baltimore_Fixed_Speed_Cameras.xlsx", sheetIndex = 1, header = TRUE)
 head(cameraData)
 
+# you can read specific rows and columns
 colIndex <- 2:3
 rowIndex <- 1:4
 cameraDataSubset <- read.xlsx("./data/Baltimore_Fixed_Speed_Cameras.xlsx", sheetIndex = 1, colIndex = colIndex, rowIndex = rowIndex)
 head(cameraDataSubset)
 
+# Generate an excel file
 write.xlsx()
-read.xlsx2() - faster but not stable when reading subsets
+read.xlsx2() # faster but not stable when reading subsets
 
+# This is more flexible and more robust package to read excel files
 XLConnect Package
 XLConnect vignette
 
-######   XML #####
-Components:
-    Markup - labesl that give the text structure 
-Content = actual text of the document
 
-Tags corresponds to general labels
-start tag  <section>
-    end tag </section>
-    empty tags <line-break />
-    Elements are specific examples of tags
-<Greeting> Hello, world </Greeting>
-    Attributes are components of the label
-<img src="jeff.jpg" alt="instructor" />
-    <step number = "3"> Connect A to B. </step>
-    
-    library(XML)
+
+##### Reading XML Files #####
+
+# XML = Extensible markup language
+# Frequently used to store structured data
+# Particularly popular in internet applications
+# Extracting XML is the basis for most web scraping
+# Components:
+#    Markup - labels that give the text structure 
+#    Content = actual text of the document
+
+# Tags corresponds to general labels
+#    start tag  <section>
+#    end tag    </section>
+#    empty tags <line-break />
+# Elements are specific examples of tags
+#    <Greeting> Hello, world </Greeting>
+# Attributes are components of the label
+#    <img src="jeff.jpg" alt="instructor" />
+#    <step number = "3"> Connect A to B. </step>
+
+# To learn more about XML
+# http://en.wikipedia.org/wiki/XML
+# http://www.w3schools.com/xml/simple.xml
+# 
+
+install.packages("XML")
+library(XML)
 fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+?xmlTreeParse #parse out the xml data
 doc <- xmlTreeParse(fileUrl, useInternal = TRUE)
-rootNode <- xmlRoot(doc)
+# After reading it is still a differnt object in R. Now we can use functions to access this object.
+# Rood node wraps the whole document and the whole document is the breakfast menu
+rootNode <- xmlRoot(doc) 
+# Get the name of the Root node
 xmlName(rootNode)
+# Get the names in the root node - the result will be a list
 names(rootNode)
+# Directly access parts of the XML document
 rootNode[[1]]
 rootNode[[1]][[1]]
+# Programatically extract certain parts of the document
 xmlSApply(rootNode, xmlValue)
 
-XPath language
-/node Top level node
-//node Node at any level
-node[@attr-name] Node with an attribute name
-node[@attr-name='bob'] Node with attribute name 'bob'
+# XPath language
+# /node Top level node
+# //node Node at any level
+# node[@attr-name] Node with an attribute name
+# node[@attr-name='bob'] Node with attribute name 'bob'
+# More information
+# http://www.stat.berkeley.edu/~statcur/Workshop2/Presentations/XML.pdf
 
+# Get items in the menu and prices
 xpathSApply(rootNode, "//name", xmlValue)
 xpathSApply(rootNode, "//price", xmlValue)
 
+# HTML Parsing
 fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
-doc <- htmlTreeParse(fileUrl, useInternal = TRUE)
-scores <- xpathSApply(doc, "//li[@class='scores']", xmlValue)
+doc <- htmlTreeParse(fileUrl, useInternal = TRUE) #useInternal will get all the nodes
+scores <- xpathSApply(doc, "//li[@class='score']", xmlValue)
 teams <- xpathSApply(doc, "//li[@class='team-name']", xmlValue)
 scores
 teams
 
+# More Information
+# http://www.omegahat.org/RSXML/shortIntro.pdf
+# http://www.omegahat.org/RSXML/Tour.pdf
+# http://www.stat.berkeley.edu/~statcur/Workshop2/Presentations/XML.pdf
 
-######   JSON   #####
 
+
+
+##### Reading JSON Files #####
+
+# Javascript object Notation
+# Light weight data storage
+# common format for data from application programming interfaces (API)
+# Similar structure to XML but different symbols
+# Data stored as
+  # Numbers (double)
+  # Strings (double quoted)
+  # Boolean (True or False)
+  # Array (ordered, comma separated enclosed in square brackets [])
+  # Objects (unordered, comma separated collection of key:value pairs in curly braces {})
+
+# http://en.wikipedia.org/wiki/JSON
+
+install.packages("jsonlite")
+install.packages('httr')
 library(jsonlite)
+library(httr)
 jsonData <- fromJSON("https://api.github.com/users/jtleek/repos")
 names(jsonData)
 names(jsonData$owner)
 jsonData$owner$login
 
+# Turn data frames into json formatted data
 myjson <- toJSON(iris, pretty = TRUE)
 cat(myjson)
 
+# Convert back to data-frame
 iris2 <- fromJSON(myjson)
 head(iris2)
 head(iris)
 
+# More information
+# http://www.json.org/
+# http://www.r-bloggers.com/new-package-jsonlite-a-smarter-json-encoderdecoder/
+# http://cran.r-project.org/web/packages/jsonlite/vignettes/json-mapping.pdf
 
-######   Data.Table   #####
 
-Inherets from data.frame
-written in C
-much faster, subset, group, update
 
+
+#####   Data.Table   #####
+
+# Faster and more memory efficient
+
+# Inherets from data.frame
+# written in C
+# much faster, subset, group, update
+
+install.packages("data.table")
 library(data.table)
 DF <- data.frame(x=rnorm(9), y=rep(c("a","b","c"), each = 3), z = rnorm(9))
-head(DF)
 head(DF,3)
 DT <- data.table(x=rnorm(9), y=rep(c("a","b","c"), each = 3), z = rnorm(9))
 head(DT,3)
 
-tables() #shows the tables in the memory
+# Display the tables in the memory
+tables() 
 
+# Subset rows
 DT[2,]
 DT[DT$y=="a",]
-
+# Subsets 2nd and 3rd rows
 DT[c(2,3)]
 
-DT[,c(2,3)]
+# Subset columns
+# This is where data.table is different than data.frame
+DT[,c(2,3)] # will not generate the result that we want
 
+# In R an expression is a collection of statements enclosed in curly brackets
+{
+  x = 1
+  y = 2
+}
+k = {print(10); 5}
+k
+
+# Calculating values for variables with expressions
+# we can pass a list of functions for calculation
+str(DT)
+summary(DT)
 DT[,list(mean(x), sum(z))]
 DT[,table(y)]
 
+# Adding new columns
+ 
 DT[,w := z^2]
 DT
 
 tables()
 
-DT2 <- DT
+DT2 <- DT # This won't generate a new copy in the memory.
 DT[,y:=2]
 head(DT, n = 3)
 head(DT2, n = 3)
-use copy function
+# use copy function to generate a new copy of the table
+
+
+# Multiple operations
+# Use statements
 
 DT[, m:={tmp <- (x+z); log2(tmp+5)}]
 DT
+
+
+# Plyr like operations
+
 DT[, a:=x>0]
 DT
-DT[, b:=mean(x+w), by = a]
+DT[, b:=mean(x+w), by = a] # get the mean with grouping with column "a"
 DT
 
+
+# Special variables
+# .N : An integer, length one, containing the number of times a particular group appears
 set.seed(123)
 DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
 DT
-DT[, .N, by=x] # count the number of times grouped by x variable
+DT[, .N, by=x] # count the number of times a grouped by x variable
 
+
+# keys
 DT <- data.table(x=rep(c("a","b","c"), each = 100), y = rnorm(300))
 DT
 setkey(DT,x)
 DT['a']
 
+
+# Use keys to join data.tables
 DT1 <- data.table(x=c("a","a","b", "dt1"), y = 1:4)
 DT2 <- data.table(x=c("a","b","dt2"), z = 5:7)
 setkey(DT1, x)
@@ -211,8 +313,13 @@ setkey(DT2, x)
 merge(DT1, DT2)
 
 
+# Fast reading
 big_df <- data.frame(x=rnorm(1e6), y =rnorm(1e6))
 file <- tempfile()
 write.table(big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
 system.time(fread(file))
 system.time(read.table(file, header=TRUE, sep="\t"))
+
+# More info:
+# http://stackoverflow.com/questions/13618488/what-you-can-do-with-data-frame-that-you-cant-in-data-table
+# https://github.com/raphg/Biostat-578/blob/master/Advanced_data_manipulation.Rpres
---
+++ GettingCleaningData/Quizes.R
@@ -0,0 +1,106 @@
+########## QUIZ 1 ##########
+
+#### 1
+# The American Community Survey distributes downloadable data about United States 
+# communities. Download the 2006 microdata survey about housing for the state of Idaho 
+# using download.file() from here: 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+# and load the data into R. The code book, describing the variable names is here: 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf  
+# How many properties are worth $1,000,000 or more?
+
+setwd("C:\\Users\\cemalper\\Documents\\Coursera\\GetData")
+getwd()
+
+if(!file.exists("Data")) {
+  dir.create("Data")
+}
+
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileurl, destfile= "Data/Quiz1.csv")
+
+list.files("./Data")
+
+housingData <- read.table("Data/Quiz1.csv", sep = ",", header = TRUE, quote = "")
+head(housingData)
+names(housingData)
+str(housingData)
+
+housingData$VAL
+length(housingData[which(housingData$VAL=="24"), ]$VAL)
+# 53
+
+
+#### 2
+# Use the data you loaded from Question 1. Consider the variable FES in the 
+# code book. Which of the "tidy data" principles does this variable violate? 
+
+housingData$FES
+summary(housingData$FES)
+
+Tidy data has one variable per column.   
+
+
+#### 3
+# Download the Excel spreadsheet on Natural Gas Aquisition Program here: 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx  
+# Read rows 18-23 and columns 7-15 into R and assign the result to a variable called:  dat 
+# What is the value of:  sum(dat$Zip*dat$Ext,na.rm=T) 
+# (original data source: http://catalog.data.gov/dataset/natural-gas-acquisition-program)
+
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx?accessType=DOWNLOAD"
+download.file(fileurl, destfile= "Data/Quiz1_NatGas.xlsx", mode = "wb")
+
+library(xlsx)
+colIndex <- 7:15
+rowIndex <- 18:23
+NGAPData <- read.xlsx("./Data/Quiz1_NatGas.xlsx", sheetIndex = 1, colIndex=colIndex, rowIndex=rowIndex)
+head(NGAPData)
+sum(NGAPData$Zip*NGAPData$Ext,na.rm=T)
+# 36534720
+
+
+#### 4
+# Read the XML data on Baltimore restaurants from here: 
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml  
+# How many restaurants have zipcode 21231? 
+
+library(XML)
+library(RCurl)
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+data <- getURL(fileurl, ssl.verifypeer = FALSE)
+doc <- xmlParse(data)
+rootNode<-xmlRoot(doc)
+xmlName(rootNode)
+xmlSApply(rootNode,xmlValue)
+a<-xpathSApply(rootNode,"//zipcode",xmlValue)
+a
+a<-a[which(a=="21231")]
+length(a)
+# 127
+
+
+#### 5
+# The American Community Survey distributes downloadable data about United States 
+# communities. Download the 2006 microdata survey about housing for the state of Idaho 
+# using download.file() from here: 
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv 
+# using the fread() command load the data into an R object  DT 
+# Which of the following is the fastest way to calculate the average value of the variable pwgtp15 
+# broken down by sex using the data.table package? 
+
+fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileurl, destfile= "Data/Quiz1_IdahoHousing.csv")
+library(data.table)
+DT <- fread("Data/Quiz1_IdahoHousing.csv")
+#rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
+DT[,mean(pwgtp15),by=SEX]
+mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)
+tapply(DT$pwgtp15,DT$SEX,mean)
+#mean(DT$pwgtp15,by=DT$SEX)
+sapply(split(DT$pwgtp15,DT$SEX),mean)
+
+system.time(DT[,mean(pwgtp15),by=SEX]) # this one is the fastest
+system.time({mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)})
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
